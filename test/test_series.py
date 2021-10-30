52cf04896e594fdaeb48aca8e56128570d366bf6
easai <aksarih@gmail.com>
2021-10-30 16:18:44 -0400

    Abstracted functions for clarity.

--- test/test_series.py
+++ test/test_series.py
@@ -3,30 +3,22 @@
 from src.series.series import *
 import pytest
 
-def f(x):
-    return x*x
 
 def test_series():
     """Test Series class def.
     """
-    seq=Series(1,4,f)
+    seq=Series(1,4,lambda x:x*x)
     assert seq.sum() == 1+4+9+16
-
-def power(x):
-    return 2**x
 
 def test_power_series():
     """Test power series
     """
-    seq=Series(0,4,power)
+    seq=Series(0,4,lambda x:2**x)
     assert seq.sum() == (1+2+4+8+16)
-
-def alternate(x):
-    return (-1)**x
 
 def test_power_series():
     """Test alternate series
     """
-    seq=Series(1,1001,alternate)
+    seq=Series(1,1001,lambda x:(-1)**x)
     print(f"sum={seq.sum()}")
     assert seq.sum() == -1
